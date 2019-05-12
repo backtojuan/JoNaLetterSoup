00ae68f9808052d9d8b015d7fb10f1629f44bbbb
Juan José Valencia <juanchouni@hotmail.com>
2019-05-12 16:37:26 -0500

    Fourth version of the software Project. GUI classes and fxml file created and being coded. Model class for manage lettersoup finished, nothing running yet

--- src/model/lettersoup/Directions.java
+++
@@ -1,5 +0,0 @@
-package model.lettersoup;
-
-public enum Directions {
-	RIGHT,LEFT,UP,DOWN,NORTHWEST,NORTHEAST,SOUTHWEST,SOUTHEAST;
-}
--- src/model/lettersoup/LetterSoup.java
+++
@@ -1,9 +0,0 @@
-	package model.lettersoup;
-
-	public class LetterSoup {
-		
-		
-		
-		
-		
-	}
